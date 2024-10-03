9375d2049e35abce79a3f29aed3f1d585be3e507
Nate Bohn <nath@nbohn.com>
2024-10-03 15:19:42 -0500

    first commit

---
+++ provider.tf
@@ -0,0 +1,15 @@
+
+
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.3"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
